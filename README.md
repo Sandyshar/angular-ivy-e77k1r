06f08897e9f1137f9d9a2abe9c0786f1b62525c5
Sandyshar <sssandeep186@gmail.com>
2021-09-10 17:36:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-e77k1r
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-e77k1r)
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import ({ Component, VERSION } from '@angular/core';
+selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+@Component
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+}
